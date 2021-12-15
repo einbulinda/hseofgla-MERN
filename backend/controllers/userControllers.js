d29b3b88793f3a519fcef3d3f2ee276a816a0cdb
einbulinda <einbulinda@gmail.com>
2021-12-15 18:22:16 +0300

    Backend for login successfully created

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,10 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModels.js";
+import generateToken from "../utils/generateToken.js";
 
+// @description     Register New User
+// @route           POST /api/users/register
+// @access          Public
 export const registerUser = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password, photo } = req.body;
 
@@ -26,9 +30,33 @@ export const registerUser = asyncHandler(async (req, res) => {
       email: newUser.email,
       isAdmin: newUser.isAdmin,
       photo: newUser.photo,
+      // token: generateToken(newUser._id),
     });
   } else {
     res.status(400);
     throw new Error("An error ocurred during registration");
+  }
+});
+
+// @description     Authenticate User
+// @route           POST /api/users/login
+// @access          Public
+export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      photo: user.photo,
+      // token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
   }
 });
